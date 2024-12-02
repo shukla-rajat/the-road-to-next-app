f0b7601cb7d94c1579cd11de9935a964a4295fde
Rajat Shukla <rajatshukla799@gmail.com>
2024-12-02 17:48:25 +0100

    Implementing Linking Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <h2 className='text-lg'> Home Page </h2>;
+  return (
+  <div>
+    <h2 className="text-lg"> Home Page </h2>
+    <Link href="/tickets" className="underline"> Go To Tickets</Link>
+  </div>
+  );
 }
 
 export default HomePage;
---
+++ src/data.ts
@@ -0,0 +1,14 @@
+export const initialTickets = [
+  {
+    id: "1",
+    title: "Ticket 1",
+    content: "This is the first Ticket",
+    status: "Done",
+  },
+  {
+    id: "2",
+    title: "Ticket 2",
+    content: "This is the second Ticket",
+    status: "Opne",
+  },
+];
