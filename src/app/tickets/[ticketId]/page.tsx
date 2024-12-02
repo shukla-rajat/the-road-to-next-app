bac2360238470b17a59626a2014fddeedc786a4b
flash5452 <flash5452@gmail.com>
2024-12-02 17:48:25 +0100

    Implementing Linking Components

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,13 +1,27 @@
+import { initialTickets } from "../../../data";
 
 type TicketPageProps = {
   params: Promise<{
     ticketId: string;
-  }>
-}
+  }>;
+};
 
-const TicketPage = async({params} : TicketPageProps) => {
-    const {ticketId} = await params;
-    return <h2 className='text-lg'> Ticket Page: {ticketId} </h2>;
+const TicketPage = async ({ params }: TicketPageProps) => {
+  const { ticketId } = await params;
+  const ticket = initialTickets.find((ticket) => {
+    return ticket.id === ticketId;
+  });
+  if (!ticket) {
+    return <h2 className="text-lg"> Ticket Not Found ! </h2>;
   }
-  
-  export default TicketPage;
+  return (
+    <div>
+      <h2 className="text-lg"> Ticket Page: {ticket.id} </h2>
+      <h2 className="text-lg"> Ticket Title: {ticket.title} </h2>
+      <h2 className="text-lg"> Ticket Content {ticket.content} </h2>
+      <h2 className="text-lg"> Ticket Status: {ticket.status} </h2>
+    </div>
+  );
+};
+
+export default TicketPage;
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import { initialTickets } from "../../data";
+
 const TicketsPage = () => {
-    return <h2 className='text-lg'> Tickets Page </h2>;
+    return (
+      <div>
+        <h2 className='text-lg'> Tickets Page </h2>
+        {
+          initialTickets.map((ticket) => (
+            <div key={ticket.id}>
+              <h2 className="text-lg">{ticket.title}</h2>
+              <Link href={`/tickets/${ticket.id}`} className="text-sm underline">View</Link>
+            </div>
+          ))
+        }
+      </div>
+    ) 
   }
   
   export default TicketsPage;
