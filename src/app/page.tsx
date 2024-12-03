2e15c8ec20b68f7badedb84e4d0b894ac7494549
Rajat Shukla <rajatshukla799@gmail.com>
2024-12-03 16:38:52 +0100

    Add layout.tsx for consistent page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
   <div>
     <h2 className="text-lg"> Home Page </h2>
-    <Link href="/tickets" className="underline"> Go To Tickets</Link>
+    <Link href={ ticketsPath() } className="underline"> Go To Tickets</Link>
   </div>
   );
 }
