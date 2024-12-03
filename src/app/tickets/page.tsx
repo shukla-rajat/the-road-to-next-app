bab3a6d45454754280bdb02948fa70177e3d6b52
flash5452 <flash5452@gmail.com>
2024-12-03 16:38:52 +0100

    Add layout.tsx for consistent page layout

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { initialTickets } from "@/data";
+import { ticketPath } from "@/paths";
 
 const TicketsPage = () => {
     return (
@@ -9,7 +10,7 @@ const TicketsPage = () => {
           initialTickets.map((ticket) => (
             <div key={ticket.id}>
               <h2 className="text-lg">{ticket.title}</h2>
-              <Link href={`/tickets/${ticket.id}`} className="text-sm underline">View</Link>
+              <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
             </div>
           ))
         }
