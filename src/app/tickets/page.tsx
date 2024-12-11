c775ab799330e92b974cf5a5e11daeb4a94078cf
flash5452 <flash5452@gmail.com>
2024-12-11 16:36:11 +0100

    added conditional tailwind css

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,33 +1,46 @@
+import clsx from "clsx";
 import Link from "next/link";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
 const TICKET_ICONS = {
-  OPEN: 'O',
-  DONE: 'X',
-  IN_PROGRESS: '>'
-}
+  OPEN: "O",
+  DONE: "X",
+  IN_PROGRESS: ">",
+};
 const TicketsPage = () => {
-    return (
-      <div className="flex-1 flex flex-col gap-y-8">
-        <div>
-          <h2 className="text-3xl font-bold tracking-tight"> Tickets Page </h2>
-          <p className="text-sm text-muted-foreground"> Your home place for tickets</p>
-        </div>
-        <div className="flex-1 flex flex-col items-center gap-y-4">
-          {
-            initialTickets.map((ticket) => (
-              <div key={ticket.id} className="w-full max-w-[420px] p-4 border border-slate-100 rounded">
-                <div>{TICKET_ICONS[ticket.status]}</div>
-                <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
-                <p className="text-sm text-slate-500 truncate">{ticket.content}</p>
-                <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
-              </div>
-            ))
-          }
-        </div>
+  return (
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight"> Tickets Page </h2>
+        <p className="text-sm text-muted-foreground">
+          {" "}
+          Your home place for tickets
+        </p>
       </div>
-    ) 
-  }
-  
-  export default TicketsPage;
+      <div className="flex-1 flex flex-col items-center gap-y-4">
+        {initialTickets.map((ticket) => (
+          <div
+            key={ticket.id}
+            className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
+          >
+            <div>{TICKET_ICONS[ticket.status]}</div>
+            <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+            <p
+              className={clsx("text-sm text-slate-500 truncate", {
+                "line-through": ticket.status === "DONE",
+              })}
+            >
+              {ticket.content}
+            </p>
+            <Link href={ticketPath(ticket.id)} className="text-sm underline">
+              View
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TicketsPage;
