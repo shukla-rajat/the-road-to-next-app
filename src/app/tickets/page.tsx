ca3e7d925306f3b1a105f35e67c0a2d3069c9364
flash5452 <flash5452@gmail.com>
2024-12-11 15:46:12 +0100

    added tailwind css styling

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -9,17 +9,23 @@ const TICKET_ICONS = {
 }
 const TicketsPage = () => {
     return (
-      <div>
-        <h2 className='text-lg'> Tickets Page </h2>
-        {
-          initialTickets.map((ticket) => (
-            <div key={ticket.id}>
-              <div>{TICKET_ICONS[ticket.status]}</div>
-              <h2 className="text-lg">{ticket.title}</h2>
-              <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
-            </div>
-          ))
-        }
+      <div className="flex-1 flex flex-col gap-y-8">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight"> Tickets Page </h2>
+          <p className="text-sm text-muted-foreground"> Your home place for tickets</p>
+        </div>
+        <div className="flex-1 flex flex-col items-center gap-y-4">
+          {
+            initialTickets.map((ticket) => (
+              <div key={ticket.id} className="w-full max-w-[420px] p-4 border border-slate-100 rounded">
+                <div>{TICKET_ICONS[ticket.status]}</div>
+                <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+                <p className="text-sm text-slate-500 truncate">{ticket.content}</p>
+                <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
+              </div>
+            ))
+          }
+        </div>
       </div>
     ) 
   }
