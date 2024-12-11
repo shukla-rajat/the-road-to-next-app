cbc270c880985b7647aaf196322224f291342d74
Rajat Shukla <rajatshukla799@gmail.com>
2024-12-11 15:46:12 +0100

    added tailwind css styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
-  <div>
-    <h2 className="text-3xl font-bold"> Home Page </h2>
-    <Link href={ ticketsPath() } className="underline"> Go To Tickets</Link>
+  <div className="flex-1 flex flex-col gap-y-8">
+    <div>
+      <h2 className="text-3xl font-bold tracking-tight"> Home Page </h2>
+      <p className="text-sm text-muted-foreground"> Your home place to start</p>
+    </div>
+    <div className="flex-1 flex flex-col items-center">
+      <Link href={ ticketsPath() } className="underline"> Go To Tickets</Link>
+    </div>
   </div>
   );
 }
