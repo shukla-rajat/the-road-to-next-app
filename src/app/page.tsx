daa21c905a0447d29c0c556dbec65dc77ba23701
Rajat Shukla <rajatshukla799@gmail.com>
2024-12-11 15:14:48 +0100

    added const assertion and started tailwind css styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { ticketsPath } from "@/paths";
 const HomePage = () => {
   return (
   <div>
-    <h2 className="text-lg"> Home Page </h2>
+    <h2 className="text-3xl font-bold"> Home Page </h2>
     <Link href={ ticketsPath() } className="underline"> Go To Tickets</Link>
   </div>
   );
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,6 +2,11 @@ import Link from "next/link";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
+const TICKET_ICONS = {
+  OPEN: 'O',
+  DONE: 'X',
+  IN_PROGRESS: '>'
+}
 const TicketsPage = () => {
     return (
       <div>
@@ -9,6 +14,7 @@ const TicketsPage = () => {
         {
           initialTickets.map((ticket) => (
             <div key={ticket.id}>
+              <div>{TICKET_ICONS[ticket.status]}</div>
               <h2 className="text-lg">{ticket.title}</h2>
               <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
             </div>
