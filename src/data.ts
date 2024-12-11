afe1563976260ff56bb80f1fb4ba740aae656d86
flash5452 <flash5452@gmail.com>
2024-12-11 15:14:48 +0100

    added const assertion and started tailwind css styling

--- src/data.ts
+++ src/data.ts
@@ -3,12 +3,18 @@ export const initialTickets = [
     id: "1",
     title: "Ticket 1",
     content: "This is the first Ticket",
-    status: "Done",
+    status: "DONE" as const,
   },
   {
     id: "2",
     title: "Ticket 2",
     content: "This is the second Ticket",
-    status: "Opne",
+    status: "OPEN" as const,
+  },
+  {
+    id: "3",
+    title: "Ticket 3",
+    content: "This is the third Ticket",
+    status: "IN_PROGRESS" as const,
   },
 ];
