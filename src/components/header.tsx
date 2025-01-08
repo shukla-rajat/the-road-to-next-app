57ecfd10f6782abb88a1112b99c366fed2843e0d
Rajat Shukla <rajatshukla799@gmail.com>
2025-01-08 18:00:30 +0100

    Introduce custom components to encapsulate and reuse UI elements for Header

---
+++ src/components/header.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { homePath, ticketsPath } from "@/paths";
+import { buttonVariants } from "@/components/ui/button";
+import { LucideKanban } from "lucide-react";
+
+const Header = () => {
+    return (
+        <>
+            <nav
+            className="
+                supports-backdrop-blur:bg-background/60
+                fixed left-0 right-0 top-0 z-20
+                border-b bg-background/95 backdrop-blur
+                w-full flex py-2.5 px-5 justify-between
+            "
+            >
+            <div>
+                <Link
+                className={buttonVariants({ variant: "ghost" })}
+                href={homePath()}
+                >
+                <LucideKanban />
+                <h1 className="ml-2 text-lg font-semibold">TicketBounty</h1>
+                </Link>
+            </div>
+            <div>
+                <Link
+                className={buttonVariants({ variant: "default" })}
+                href={ticketsPath()}
+                >
+                Tickets
+                </Link>
+            </div>
+            </nav>
+        </>
+    )
+}
+
+export {Header};
