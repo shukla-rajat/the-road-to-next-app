dfbd37a9a1cb49c3d9ff8591ce98d2cd6eb45788
Rajat Shukla <rajatshukla799@gmail.com>
2025-01-23 15:26:20 +0100

    extracted component, type and constants related to Ticket item in features folder

---
+++ src/features/ticket/constants.tsx
@@ -0,0 +1,7 @@
+import { LucideFileText, LucideCircleCheck, LucidePencil } from "lucide-react";
+
+export const TICKET_ICONS = {
+    OPEN: <LucideFileText />,
+    DONE: <LucideCircleCheck />,
+    IN_PROGRESS: <LucidePencil />,
+  };
