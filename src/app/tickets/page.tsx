8779fcce930eb802b3d58b3602c684dcc221beb6
Rajat Shukla <rajatshukla799@gmail.com>
2025-02-05 17:45:19 +0100

    data fetching in client components

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,13 +1,29 @@
-import { initialTickets } from "@/data";
+"use client";
+
+import { useState, useEffect } from "react"; 
 import { Heading } from "@/components/heading";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
+import { Ticket } from "@/features/ticket/types";
+import { getTickets } from "@/features/ticket/queries/get-tickets";
 
 const TicketsPage = () => {
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    const fetchTickets = async() => {
+      const result = await getTickets();
+
+      setTickets(result);
+    }
+
+    fetchTickets();
+  },[]);
+  
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets Page" description="Your home place for tickets" />
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket}/>
         ))}
       </div>
