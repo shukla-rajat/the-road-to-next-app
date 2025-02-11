d91a48bed770ee3e21ce840fbc8c6e9e4aa6a8a0
flash5452 <flash5452@gmail.com>
2025-02-11 17:29:22 +0100

    implemnted fallback with a spinner component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,12 +1,13 @@
 import { Heading } from "@/components/heading";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { Suspense } from "react";
+import { Spinner } from "@/components/spinner";
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets Page" description="Your home place for tickets" />
-      <Suspense>
+      <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
     </div>
