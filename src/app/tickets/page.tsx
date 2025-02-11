71a7250d87c4cbe31385fea1b654911e616ba8d0
Rajat Shukla <rajatshukla799@gmail.com>
2025-02-11 17:10:56 +0100

    implementing streaming using suspense on list of tickets on ticket page

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,20 +1,14 @@
-import { useState, useEffect } from "react"; 
 import { Heading } from "@/components/heading";
-import { TicketItem } from "@/features/ticket/components/ticket-item";
-import { Ticket } from "@/features/ticket/types";
-import { getTickets } from "@/features/ticket/queries/get-tickets";
+import { TicketList } from "@/features/ticket/components/ticket-list";
+import { Suspense } from "react";
 
-const TicketsPage = async () => {
-  const tickets = await getTickets();
-  
+const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets Page" description="Your home place for tickets" />
-      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {tickets.map((ticket) => (
-          <TicketItem key={ticket.id} ticket={ticket}/>
-        ))}
-      </div>
+      <Suspense>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
