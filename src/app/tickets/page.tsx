5cd6e7bd2674a434ee96fb438d028e8a1613a969
Rajat Shukla <rajatshukla799@gmail.com>
2025-02-13 16:27:58 +0100

    Implemented Error Boundry component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,14 +2,18 @@ import { Heading } from "@/components/heading";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { Suspense } from "react";
 import { Spinner } from "@/components/spinner";
+import { ErrorBoundary } from "react-error-boundary";
+import { Placeholder } from '@/components/placeholder'
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets Page" description="Your home place for tickets" />
-      <Suspense fallback={<Spinner />}>
-        <TicketList />
-      </Suspense>
+      <ErrorBoundary fallback={<Placeholder label="Something went wrong!"/>}>
+        <Suspense fallback={<Spinner />}>
+          <TicketList />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
