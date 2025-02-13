d8d22a11cd549d156b5817413f86fb83849b67ae
Rajat Shukla <rajatshukla799@gmail.com>
2025-02-13 15:41:50 +0100

    placed the message in a label in a placeholder component

--- src/app/tickets/error.tsx
+++ src/app/tickets/error.tsx
@@ -1,5 +1,6 @@
 "use client"
+import { Placeholder } from "@/components/placeholder";
 
 export default function Error({error} : {error: Error}) {
-    return <div>{error.message || "Something went wrong !"}</div>
+    return <Placeholder label={error.message || "Something went wrong !"} />
 }
