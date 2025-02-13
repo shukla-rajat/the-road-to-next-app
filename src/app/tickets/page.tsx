ca9802cf9867cd6fa409505834c77d32d0b4de7a
flash5452 <flash5452@gmail.com>
2025-02-13 17:30:16 +0100

    Implemented and Removed error boundary component and Implemented not found route

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,18 +2,14 @@ import { Heading } from "@/components/heading";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { Suspense } from "react";
 import { Spinner } from "@/components/spinner";
-import { ErrorBoundary } from "react-error-boundary";
-import { Placeholder } from '@/components/placeholder'
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets Page" description="Your home place for tickets" />
-      <ErrorBoundary fallback={<Placeholder label="Something went wrong!"/>}>
-        <Suspense fallback={<Spinner />}>
-          <TicketList />
-        </Suspense>
-      </ErrorBoundary>
+      <Suspense fallback={<Spinner />}>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
