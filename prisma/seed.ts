d5f8a57528a3f3eab929b681ae9ab24c1b5b506c
Rajat Shukla <rajatshukla799@gmail.com>
2025-02-19 16:48:07 +0100

    Implement database seeding with initial data

---
+++ prisma/seed.ts
@@ -0,0 +1,50 @@
+import { PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const tickets = [
+    {
+      title: "Ticket 1",
+      content: "This is the first ticket from the database.",
+      status: "DONE" as const,
+    },
+    {
+      title: "Ticket 2",
+      content: "This is the second ticket from the database.",
+      status: "OPEN" as const,
+    },
+    {
+      title: "Ticket 3",
+      content: "This is the third ticket from the database.",
+      status: "IN_PROGRESS" as const,
+    },
+  ];
+  
+const seed = async () => {
+    /*for (const ticket of tickets) {
+        await prisma.ticket.create({
+        data: ticket,
+        });
+    }*/
+
+    /*const promises = tickets.map(ticket => {
+        prisma.ticket.create({
+            data:ticket,
+        })
+    })
+    await Promise.all(promises);*/
+
+    const t0 = performance.now();
+    console.log('DB seed: Started...');
+    
+    await prisma.ticket.deleteMany();
+
+    await prisma.ticket.createMany({
+        data:tickets
+    });
+
+    const t1 = performance.now();
+    console.log(`DB seed: Finished (${t1 - t0}ms)`);
+};
+
+seed();
