910620b23bc30f513e6d8af2d9793a384047ec70
Rajat Shukla <rajatshukla799@gmail.com>
2025-02-27 16:40:54 +0100

    abstracted card separately as independent custom component

---
+++ src/components/card-compact.tsx
@@ -0,0 +1,25 @@
+
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+
+type CardCompactProps = {
+    title:string;
+    description: string;
+    content: React.ReactNode;
+    className?: string;
+    footer?: React.ReactNode; 
+}
+
+const CardCompact = ({title, description, content, className, footer}: CardCompactProps) => {
+    return (
+        <Card className={className}>
+        <CardHeader>
+          <CardTitle> {title} </CardTitle>
+          <CardDescription>{description} </CardDescription>
+        </CardHeader>
+        <CardContent> {content} </CardContent>
+        {footer && <CardFooter>{footer}</CardFooter>}
+      </Card>
+    )
+}
+
+export { CardCompact };
