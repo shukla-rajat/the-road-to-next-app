0e04f40dbfa7e05db658b5343c81a92b051baaf9
flash5452 <flash5452@gmail.com>
2025-02-27 16:40:54 +0100

    abstracted card separately as independent custom component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,24 +2,18 @@ import { Heading } from "@/components/heading";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { Suspense } from "react";
 import { Spinner } from "@/components/spinner";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import { CardCompact } from "@/components/card-compact";
 import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets Page" description="Your home place for tickets" />
-      <Card className="w-full max-w-[420px] self-center">
-        <CardHeader>
-          <CardTitle>
-            Create Ticket
-          </CardTitle>
-          <CardDescription>
-            A new ticket will be created
-          </CardDescription>
-        </CardHeader>
-        <CardContent> <TicketCreateForm/> </CardContent>
-      </Card>
+      <CardCompact title="Create Ticket" 
+        description="A new ticket will be created" 
+        className="w-full max-w-[420px] self-center" 
+        content={<TicketCreateForm />} 
+      />
       <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
