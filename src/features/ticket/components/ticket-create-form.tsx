b01dc6b27de77573d8c3f77ec4f268e48687d65d
flash5452 <flash5452@gmail.com>
2025-02-27 15:31:42 +0100

    continued creating form using shacn compnents and connecting it to database by using prisma

--- src/features/ticket/components/ticket-create-form.tsx
+++ src/features/ticket/components/ticket-create-form.tsx
@@ -1,13 +1,21 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { createTicket } from "@/features/ticket/actions/create-ticket";
+
 const TicketCreateForm = () => {
+
+
     return (
-        <form action="" className="flex flex-col gap-y-2">
-            <label htmlFor="title">Title</label>
-            <input id="title" name="title" type="text" />
+        <form action={createTicket} className="flex flex-col gap-y-2">
+            <Label htmlFor="title">Title</Label>
+            <Input id="title" name="title" type="text" />
 
-            <label htmlFor="content">Content</label>
-            <textarea id="content" name="content" />
+            <Label htmlFor="content">Content</Label>
+            <Textarea id="content" name="content" />
 
-            <button type="submit">Create</button>
+             <Button type="submit">Create</Button>
         </form>
     )
 }
