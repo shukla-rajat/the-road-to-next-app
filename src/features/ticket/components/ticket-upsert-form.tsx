e69759616e238d479c4bbde5b2d045521c2af79d
Rajat Shukla <rajatshukla799@gmail.com>
2025-03-03 16:52:06 +0100

    Implement useTransition hook for smoother UI updates

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,18 +1,29 @@
+"use client";
+
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { upsertTicket } from "@/features/ticket/actions/upsert-ticket";
 import { Ticket } from "@prisma/client";
+import { useTransition } from "react";
+import { LucideLoaderCircle } from "lucide-react";
 
 type TicketUpsertFormProps = {
     ticket?: Ticket;
 }
 
 const TicketUpsertForm = ({ticket}: TicketUpsertFormProps) => {
+    const [isPending, startTransition] = useTransition();
 
+    const upsertTicketAction = (formData : FormData) => {
+        startTransition(async() => {
+            await upsertTicket.bind(null, ticket?.id)(formData);
+        })
+    }
+    
     return (
-        <form action={upsertTicket.bind(null, ticket?.id)} className="flex flex-col gap-y-2">
+        <form action={upsertTicketAction} className="flex flex-col gap-y-2">
 
             <Label htmlFor="title">Title</Label>
             <Input id="title" name="title" type="text" defaultValue={ticket?.title}/>
@@ -20,7 +31,9 @@ const TicketUpsertForm = ({ticket}: TicketUpsertFormProps) => {
             <Label htmlFor="content">Content</Label>
             <Textarea id="content" name="content" defaultValue={ticket?.content}/>
 
-             <Button type="submit">{ticket ? "Edit" : "Create"}</Button>
+             <Button disabled={isPending} type="submit">{ticket ? "Edit" : "Create"}
+                {isPending && (<LucideLoaderCircle className="mr-2 h-4 w-4 animate-spin"/>)}
+             </Button>
         </form>
     )
 }
