dbd01e33cc4a857bad2570ab27334c2c4ab2788c
shukla-rajat <rajatshukla799@gmail.com>
2025-03-04 15:06:17 +0100

    Replace useFormStatus with useTransition

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -6,24 +6,31 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { upsertTicket } from "@/features/ticket/actions/upsert-ticket";
 import { Ticket } from "@prisma/client";
-import { useTransition } from "react";
+import { useFormStatus } from "react-dom";
 import { LucideLoaderCircle } from "lucide-react";
+
+
+type SubmitButtonProps = {
+    label: string;
+};
+
+const SubmitButton = ({label}: SubmitButtonProps) => {
+    const {pending} = useFormStatus();
+    return (
+        <Button disabled={pending} type="submit">
+            {pending && (<LucideLoaderCircle className="mr-2 h-4 w-4 animate-spin"/>)}
+            {label}
+        </Button>
+    )    
+}
 
 type TicketUpsertFormProps = {
     ticket?: Ticket;
-}
+};
 
 const TicketUpsertForm = ({ticket}: TicketUpsertFormProps) => {
-    const [isPending, startTransition] = useTransition();
-
-    const upsertTicketAction = (formData : FormData) => {
-        startTransition(async() => {
-            await upsertTicket.bind(null, ticket?.id)(formData);
-        })
-    }
-    
     return (
-        <form action={upsertTicketAction} className="flex flex-col gap-y-2">
+        <form action={upsertTicket.bind(null, ticket?.id)} className="flex flex-col gap-y-2">
 
             <Label htmlFor="title">Title</Label>
             <Input id="title" name="title" type="text" defaultValue={ticket?.title}/>
@@ -31,9 +38,7 @@ const TicketUpsertForm = ({ticket}: TicketUpsertFormProps) => {
             <Label htmlFor="content">Content</Label>
             <Textarea id="content" name="content" defaultValue={ticket?.content}/>
 
-             <Button disabled={isPending} type="submit">{ticket ? "Edit" : "Create"}
-                {isPending && (<LucideLoaderCircle className="mr-2 h-4 w-4 animate-spin"/>)}
-             </Button>
+             <SubmitButton label= {ticket ? "Edit" : "Create"}/>
         </form>
     )
 }
