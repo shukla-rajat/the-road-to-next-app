b3a3c7c50b2abb8ee263f75d9c9ed8ba8315d874
shukla-rajat <rajatshukla799@gmail.com>
2025-03-05 11:33:28 +0100

    Improve form validation with Zod:
    - Implemented Zod schemas to validate user input in the registration form.
    - This ensures data integrity and provides more informative error messages to the user.

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -4,30 +4,42 @@ import { redirect } from "next/navigation";
 import { ticketsPath, ticketPath } from "@/paths";
 import { revalidatePath } from "next/cache";
 import { prisma } from "@/lib/prisma";
+import { z } from "zod";
+
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+});
 
 export const upsertTicket = async (
   id: string | undefined,
-  _actionState: { message: string},
+  _actionState: { message: string },
   formData: FormData
 ) => {
-  const data = {
-    title: formData.get("title") as string,
-    content: formData.get("content") as string,
-  };
-
-  await prisma.ticket.upsert({
-    where: {
-      id: id || "",
-    },
-    update: data,
-    create: data,
-  });
-
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get("title"),
+      content: formData.get("content"),
+    });
+  
+    await prisma.ticket.upsert({
+      where: {
+        id: id || "",
+      },
+      update: data,
+      create: data,
+    });
+  
+  }
+  catch(error){
+    return { message: "Something went wrong" };
+  }
+  
   revalidatePath(ticketsPath());
 
   if (id) {
     redirect(ticketPath(id));
   }
 
-  return {message:"Ticket created"};
+  return { message: "Ticket created" };
 };
