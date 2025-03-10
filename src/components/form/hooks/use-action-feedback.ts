a0cd2a46ec5d91754da2ae7fbb7b940332f5ba42
shukla-rajat <rajatshukla799@gmail.com>
2025-03-10 16:18:15 +0100

    Debug: Trigger useEffect on form updates using timestamp and useRef- Implemented timestamp and useRef to detect changes in form data.- Triggered useEffect only when form data updates, improving performance

--- src/components/form/hooks/use-action-feedback.ts
+++ src/components/form/hooks/use-action-feedback.ts
@@ -1,26 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { ActionState } from "../utils/to-action-state";
 
-type OnArgs = { actionState: ActionState }
+type OnArgs = { actionState: ActionState };
 
 type UseActionFeedbackOptions = {
-    onSuccess?: ( onArgs: OnArgs) => void;
-    onError?: ( onArgs: OnArgs) => void;
+  onSuccess?: (onArgs: OnArgs) => void;
+  onError?: (onArgs: OnArgs) => void;
 };
 
 const useActionFeedback = (
-    actionState: ActionState, 
-    options: UseActionFeedbackOptions
+  actionState: ActionState,
+  options: UseActionFeedbackOptions
 ) => {
-    useEffect(() => {
-        if(actionState.status === "SUCCESS") {
-            options.onSuccess?.({actionState});
-        }
-    
-        if(actionState.status === "ERROR") {
-            options.onError?.({actionState});
-        }
-    },[actionState, options]);
-}
+  const prevTimestamp = useRef(actionState.timestamp);
+  const isUpdate = prevTimestamp.current !== actionState.timestamp;
+
+  useEffect(() => {
+    if (!isUpdate) return;
+
+    if (actionState.status === "SUCCESS") {
+      options.onSuccess?.({ actionState });
+    }
+
+    if (actionState.status === "ERROR") {
+      options.onError?.({ actionState });
+    }
+
+    prevTimestamp.current = actionState.timestamp;
+  }, [isUpdate, actionState, options]);
+};
 
 export { useActionFeedback };
