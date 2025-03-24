12b3783a6d57900d6710386f140f7968f3ccbb60
shukla-rajat <rajatshukla799@gmail.com>
2025-03-24 18:22:44 +0100

    Update ticket status in dropdown menu

---
+++ src/features/ticket/actions/update-ticket-status.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { TicketStatus } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { ticketsPath } from "@/paths";
+import { toActionState, fromErrorToActionState } from "@/components/form/utils/to-action-state";
+
+export const updateTicketStatus = async (id: string, status: TicketStatus) => {
+    try {
+        await prisma.ticket.update({
+            where: {
+                id,
+            },
+            data: {
+                status
+            },
+        })
+    }
+    catch (error) {
+        return fromErrorToActionState(error);
+    }
+;
+
+    revalidatePath(ticketsPath());
+
+    return toActionState("SUCCESS", "Status updated")
+}
