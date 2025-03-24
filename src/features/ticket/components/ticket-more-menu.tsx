c733a9ca50640486353b42c77de5cc22fff3c4af
shukla-rajat <rajatshukla799@gmail.com>
2025-03-24 17:00:44 +0100

    Populate ticket status in dropdown menu
    - Fetched and displayed ticket status options in the dropdown menu.
    - Ensured accurate representation of available ticket statuses.

--- src/features/ticket/components/ticket-more-menu.tsx
+++ src/features/ticket/components/ticket-more-menu.tsx
@@ -1,36 +1,49 @@
-
-import { Ticket } from "@prisma/client";
+import { Ticket, TicketStatus } from "@prisma/client";
 import { LucideTrash } from "lucide-react";
-
-import { 
-    DropdownMenuTrigger, 
-    DropdownMenu, 
-    DropdownMenuContent, 
-    DropdownMenuItem 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { TICKET_STATUS_LABELS } from "../constants";
 
 type TicketMoreMenuProps = {
-    ticket: Ticket;
-    trigger: React.ReactNode;
-}
+  ticket: Ticket;
+  trigger: React.ReactElement;
+};
 
 const TicketMoreMenu = ({ ticket, trigger }: TicketMoreMenuProps) => {
-    const deleteButton = (
-        <DropdownMenuItem>
-            <LucideTrash className="mr-2 h-4 w-4" />
-            <span>Delete</span>
-        </DropdownMenuItem>
-    );
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                {trigger}
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56" side="right">
-                {deleteButton}
-            </DropdownMenuContent>
-        </DropdownMenu>
-    );
-}
+  const deleteButton = (
+    <DropdownMenuItem>
+      <LucideTrash className="h-4 w-4" />
+      <span>Delete</span>
+    </DropdownMenuItem>
+  );
+
+  const ticketStatusRadioGroupItems = (
+    <DropdownMenuRadioGroup value={ticket.status}>
+      {(Object.keys(TICKET_STATUS_LABELS) as Array<TicketStatus>).map((key) => (
+        <DropdownMenuRadioItem key={key} value={key}>
+          {TICKET_STATUS_LABELS[key]}
+        </DropdownMenuRadioItem>
+      ))}
+    </DropdownMenuRadioGroup>
+  );
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" side="right">
+        {ticketStatusRadioGroupItems}
+        <DropdownMenuSeparator />
+        {deleteButton}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 export { TicketMoreMenu };
--- src/features/ticket/constants.tsx
+++ src/features/ticket/constants.tsx
@@ -1,7 +1,13 @@
 import { LucideFileText, LucideCircleCheck, LucidePencil } from "lucide-react";
 
 export const TICKET_ICONS = {
-    OPEN: <LucideFileText />,
-    DONE: <LucideCircleCheck />,
-    IN_PROGRESS: <LucidePencil />,
-  };
+  OPEN: <LucideFileText />,
+  DONE: <LucideCircleCheck />,
+  IN_PROGRESS: <LucidePencil />,
+};
+
+export const TICKET_STATUS_LABELS = {
+  OPEN: "Open",
+  DONE: "Done",
+  IN_PROGRESS: "In Progress",
+};
