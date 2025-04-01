7416bf62dc92d560a650d29bcd44384074ada773
shukla-rajat <rajatshukla799@gmail.com>
2025-04-01 17:53:56 +0200

    Finalize user signup flow: database insertion and  session creation with cookies

--- src/features/auth/actions/sign-up.ts
+++ src/features/auth/actions/sign-up.ts
@@ -1,8 +1,14 @@
 
 "use server";
 
+import { cookies } from "next/headers";
+import { hash } from '@node-rs/argon2';
 import { ActionState, fromErrorToActionState, toActionState } from "@/components/form/utils/to-action-state";
 import { z } from "zod";
+import { prisma } from "@/lib/prisma";
+import { lucia } from '@/lib/lucia';
+import { ticketsPath } from "@/paths";
+import { redirect } from "next/navigation";
 
 const signUpSchema = z.object ({
     username: z
@@ -29,11 +35,26 @@ export const signUp = async (_actionState: ActionState, formData: FormData) => {
         const { username, email, password } = signUpSchema.parse(
             Object.fromEntries(formData)
         );
+
+        const passwordHash = await hash(password);
+
+        const user = await prisma.user.create({
+            data: {
+                username,
+                email,
+                passwordHash, 
+            }
+        });
+
+        const session = await lucia.createSession(user.id, {});
+        const sessionCookie = lucia.createSessionCookie(session.id);
+
+        (await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
     }
     catch (error) {
         return fromErrorToActionState(error, formData);
     }
 
-    return toActionState("SUCCESS", "Sign up successful")
+    redirect(ticketsPath());
     
 }
