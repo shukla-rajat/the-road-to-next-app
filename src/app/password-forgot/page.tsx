82cd3ffc36f10e0a5bd30e20b91b96f6a2e2695e
shukla-rajat <rajatshukla799@gmail.com>
2025-04-01 18:33:06 +0200

    Implemented sign-in form with validation, routes, and database integration.

---
+++ src/app/password-forgot/page.tsx
@@ -0,0 +1,6 @@
+
+const ForgotPasswordPage = () => {
+    return <h2 className="text-lg">ForgotPasswordPAge</h2>
+};
+
+export default ForgotPasswordPage;
--- src/features/auth/actions/sign-in.ts
+++ src/features/auth/actions/sign-in.ts
@@ -8,6 +8,8 @@ import { z } from "zod";
 import { redirect } from "next/navigation";
 import { ticketsPath } from "@/paths";
 import { prisma } from "@/lib/prisma";
+import { cookies } from "next/headers";
+import { lucia } from "@/lib/lucia";
 
 const signUpSchema = z.object ({
     email: z.string().min(1, { message: "Is required"}).max(191).email(),
@@ -33,6 +35,11 @@ export const signIn = async (_actionState: ActionState, formData: FormData) => {
         if(!validPassword) {
             return toActionState("ERROR", "Incorrect email or password");
         }
+
+        const session = await lucia.createSession(user.id, {});
+        const sessionCookie = lucia.createSessionCookie(session.id);
+
+        (await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
     }
     catch (error) {
         return fromErrorToActionState(error, formData);
