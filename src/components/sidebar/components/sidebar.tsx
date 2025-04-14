710e4a80834cc9132781a982dd5ede20664f57bb
shukla-rajat <rajatshukla799@gmail.com>
2025-04-14 14:39:35 +0200

    Implement closest sidebar option detection using Levenshtein distance.
    - Integrated the Levenshtein distance algorithm to identify the sidebar option with the closest URL match to the current route.
    - Automatically highlights the most relevant sidebar item based on the current page.

--- src/components/sidebar/components/sidebar.tsx
+++ src/components/sidebar/components/sidebar.tsx
@@ -1,16 +1,27 @@
 
 "use client";
 
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 import { useAuth } from "@/features/auth/hooks/use-auth";
 import { cn } from "@/lib/utils";
+import { signInPath,signUpPath } from "@/paths";
+import { getActivePath } from "@/utils/get-active-path";
 
 import { navItems } from "../constants";
 import { SidebarItem } from "./sidebar-item";
 
 const Sidebar = () => {
     const { user, isFetched } = useAuth();
+    const pathName = usePathname();
+
+    const { activeIndex } = getActivePath(
+        pathName,
+        navItems.map((item) => item.href),
+        [signInPath(), signUpPath()]
+    );
+
     const [isTransition, setTransition] = useState(false);
     const [isOpen, setOpen] = useState(false);
 
@@ -37,10 +48,11 @@ const Sidebar = () => {
         >
             <div className="px-3 py-2">
             <nav className="space-y-2">
-                {navItems.map((navItem) => (
+                {navItems.map((navItem, index) => (
                     <SidebarItem
                         key={navItem.title}
                         isOpen={isOpen}
+                        isActive = {activeIndex === index}
                         navItem={navItem}
                     />
                 ))}
