4cc7e3348859ca83bcc4efd4e7acbe409ebfdba5
shukla-rajat <rajatshukla799@gmail.com>
2025-04-29 16:08:07 +0200

    Display comments on ticket detail page.
    - Implemented functionality to fetch and display a list of comments associated with the currently viewed ticket.
    - Modified the ticket detail page to render the retrieved comments, including author information.
    - Provides users with the ability to see the discussion related to a specific ticket.

---
+++ src/features/comment/queries/get-comments.ts
@@ -0,0 +1,20 @@
+
+import { prisma } from "@/lib/prisma";
+
+export const getComments = async (ticketId: string) => {
+    return await prisma.comment.findMany({
+        where: {
+            ticketId,
+        },
+        include: {
+            user: {
+                select: {
+                    username: true,
+                },
+            },
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+}
