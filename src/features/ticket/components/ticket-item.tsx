7f8c448d69d964d3bb318013b3d9a8c19cb7dd9d
shukla-rajat <rajatshukla799@gmail.com>
2025-05-01 11:18:13 +0200

    Implement Shadcn UI Skeleton for sequential data loading.
    - Replaced the generic spinner with Shadcn UI's Skeleton component to provide a more informative visual representation of sequential data fetching.
    - Implemented multiple skeleton states to reflect the loading of different data sections.

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,102 +1,136 @@
-
 import { Prisma } from "@prisma/client";
 import clsx from "clsx";
-import { LucideMoreVertical,LucidePencil, LucideSquareArrowOutUpRight } from "lucide-react";
+import {
+  LucideMoreVertical,
+  LucidePencil,
+  LucideSquareArrowOutUpRight,
+} from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter,CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { getAuth } from "@/features/auth/queries/get-auth";
 import { isOwner } from "@/features/auth/utils/is-owner";
 import { Comments } from "@/features/comment/components/comments";
-import { ticketEditPath,ticketPath } from "@/paths";
+import { ticketEditPath, ticketPath } from "@/paths";
 import { toCurrencyFromCent } from "@/utils/currency";
 
 import { TICKET_ICONS } from "../constants";
 import { TicketMoreMenu } from "./ticket-more-menu";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type TicketItemProps = {
-    ticket: Prisma.TicketGetPayload<{ include: { 
-        user: {
-            select: {
-                username: true;
-            }
-        }
+  ticket: Prisma.TicketGetPayload<{
+    include: {
+      user: {
+        select: {
+          username: true;
+        };
+      };
     };
-}>;
-    isDetail?: boolean;
+  }>;
+  isDetail?: boolean;
 };
 
 const TicketItem = async ({ ticket, isDetail }: TicketItemProps) => {
-    const { user } = await getAuth();
-    const isTicketOwner = isOwner(user, ticket);
+  const { user } = await getAuth();
+  const isTicketOwner = isOwner(user, ticket);
 
-    const detailButton = (
-        <Button variant="outline" size="icon" asChild>
-            <Link prefetch href={ticketPath(ticket.id)}><LucideSquareArrowOutUpRight className="h-4 w-4" /></Link>
+  const detailButton = (
+    <Button variant="outline" size="icon" asChild>
+      <Link prefetch href={ticketPath(ticket.id)}>
+        <LucideSquareArrowOutUpRight className="h-4 w-4" />
+      </Link>
+    </Button>
+  );
+
+  const editButton = isTicketOwner ? (
+    <Button variant="outline" size="icon" asChild>
+      <Link prefetch href={ticketEditPath(ticket.id)}>
+        <LucidePencil className="h-4 w-4" />
+      </Link>
+    </Button>
+  ) : null;
+
+  const moreMenu = isTicketOwner ? (
+    <TicketMoreMenu
+      ticket={ticket}
+      trigger={
+        <Button variant="outline" size="icon">
+          <LucideMoreVertical className="h-4 w-4" />
         </Button>
-    );
+      }
+    />
+  ) : null;
 
-    const editButton = isTicketOwner ? (
-        <Button variant="outline" size="icon" asChild>
-            <Link prefetch href={ticketEditPath(ticket.id)}>
-                <LucidePencil className="h-4 w-4" />
-            </Link>
-        </Button>
-    ) : null;
+  return (
+    <div
+      className={clsx("w-full flex flex-col gap-y-4", {
+        "max-w-[580px]": isDetail,
+        "max-w-[420px]": !isDetail,
+      })}
+    >
+      <div className="flex gap-x-2">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle className="flex gap-x-2">
+              <span>{TICKET_ICONS[ticket.status]}</span>
+              <span className="truncate">{ticket.title}</span>
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <span
+              className={clsx("whitespace-break-spaces", {
+                "line-clamp-3": !isDetail,
+              })}
+            >
+              {ticket.content}
+            </span>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <p className="text-sm text-muted-foreground">
+              {ticket.deadline} by {ticket.user.username}
+            </p>
+            <p className="text-sm text-muted-foreground">
+              {toCurrencyFromCent(ticket.bounty)}
+            </p>
+          </CardFooter>
+        </Card>
 
-    const moreMenu = isTicketOwner ? ( 
-        <TicketMoreMenu ticket={ticket} trigger={
-            <Button variant="outline" size="icon">
-                <LucideMoreVertical className="h-4 w-4" />
-            </Button>
-            } 
-        />
-    ) : null;
-
-    return (
-        <div className={clsx("w-full flex flex-col gap-y-4", {
-            "max-w-[580px]": isDetail,
-            "max-w-[420px]": !isDetail
-        })}>
-            <div className="flex gap-x-2">
-                <Card className="w-full">
-                    <CardHeader>
-                        <CardTitle className="flex gap-x-2">
-                            <span>{TICKET_ICONS[ticket.status]}</span>
-                            <span className="truncate">{ticket.title}</span>
-                        </CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <span className={clsx("whitespace-break-spaces", {
-                            "line-clamp-3": !isDetail,
-                        })}>
-                            {ticket.content}
-                        </span>
-                    </CardContent>
-                    <CardFooter className="flex justify-between">
-                        <p className="text-sm text-muted-foreground">{ticket.deadline} by {ticket.user.username}</p>
-                        <p className="text-sm text-muted-foreground">{toCurrencyFromCent(ticket.bounty)}</p>
-                    </CardFooter>
-                </Card>
-
-                <div className="flex flex-col gap-y-1">
-                    {isDetail ? (
-                        <>
-                            {editButton}
-                            {moreMenu}
-                        </>
-                    ) : (
-                        <>
-                            {detailButton}
-                            {editButton}
-                        </>
-                    )}
-                </div>
-            </div>
-            { isDetail ? <Comments ticketId={ticket.id} /> : null }
+        <div className="flex flex-col gap-y-1">
+          {isDetail ? (
+            <>
+              {editButton}
+              {moreMenu}
+            </>
+          ) : (
+            <>
+              {detailButton}
+              {editButton}
+            </>
+          )}
         </div>
-    );
-}
+      </div>
+      {isDetail ? (
+        <Suspense fallback={
+            <div className="flex flex-col gap-y-4">
+                <Skeleton className="h-[250px] w-full"/>
+                <Skeleton className="h-[80px] ml-8" />
+                <Skeleton className="h-[80px] ml-8"/>
+            </div>
+        }>
+            <Comments ticketId={ticket.id} />
+        </Suspense>
+      ) : null}
+    </div>
+  );
+};
 
 export { TicketItem };
