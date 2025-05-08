50f9273e30a2e419f63713fdc07db0b61a066c59
shukla-rajat <rajatshukla799@gmail.com>
2025-05-08 16:13:52 +0200

    Feat: Install and configure React Query.
    - Installed the eact-query library.
    - Wrapped the application with a ReactQueryProvider to make React Query functionality available throughout the component tree.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,15 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { NuqsAdapter } from 'nuqs/adapters/next/app';
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
-import {Header} from "@/app/_navigation/header";
+import { Header } from "@/app/_navigation/header";
 import { Sidebar } from "@/app/_navigation/sidebar/components/sidebar";
 import { RedirectToast } from "@/components/redirect-toast";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+
+import { ReactQueryProvider } from "./_providers/react-query/react-query-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -38,22 +40,24 @@ export default function RootLayout({
       >
         <NuqsAdapter>
           <ThemeProvider>
-            <Header />
-            <div className="flex h-screen overflow-hidden border-collapse">
-              <Sidebar />
-              <main
-                className="
+            <ReactQueryProvider>
+              <Header />
+              <div className="flex h-screen overflow-hidden border-collapse">
+                <Sidebar />
+                <main
+                  className="
                   min-h-screen flex-1
                   overflow-y-auto overflow-x-hidden
                   py-24 px-8
                   bg-secondary/20
                   flex flex-col
                 "
-              >
-                {children}
-              </main>
-            </div>
-            <Toaster expand/>
+                >
+                  {children}
+                </main>
+              </div>
+              <Toaster expand />
+            </ReactQueryProvider>
             <RedirectToast />
           </ThemeProvider>
         </NuqsAdapter>
