f288cde6fbbd7866cb3df9da6b7a8b98215628ee
shukla-rajat <rajatshukla799@gmail.com>
2026-03-12 16:19:33 +0100

    feat(auth): implement password reset functionality
    Add token ID to hash conversion and database verification
    
    Enforce one-time use for reset tokens
    
    Handle expired or missing token errors gracefully
    
    Hash new user password and update database record

--- src/features/password/action/password-reset.ts
+++ src/features/password/action/password-reset.ts
@@ -7,8 +7,13 @@ import { setCookieByKey } from "@/actions/cookies";
 import {
   ActionState,
   fromErrorToActionState,
+  toActionState,
 } from "@/components/form/utils/to-action-state";
+import { prisma } from "@/lib/prisma";
 import { signInPath } from "@/paths";
+import { hashToken } from "@/utils/crypto";
+
+import { hashPassword } from "../utils/hash-and-verify";
 
 const passwordResetSchema = z
   .object({
@@ -35,7 +40,48 @@ export const passwordReset = async (
       confirmPassword: formData.get("confirmPassword"),
     });
 
-    //TODO Implement password reset feature
+    const tokenHash = hashToken(tokenId);
+
+    const passwordResetToken = await prisma.passwordResetToken.findUnique({
+      where: {
+        tokenHash,
+      },
+    });
+
+    if (passwordResetToken) {
+      await prisma.passwordResetToken.delete({
+        where: {
+          tokenHash,
+        },
+      });
+    }
+
+    if (
+      !passwordResetToken ||
+      Date.now() > passwordResetToken.expiresAt.getTime()
+    ) {
+      return toActionState(
+        "ERROR",
+        "Expired or invalid verification token",
+        formData,
+      );
+    }
+
+    await prisma.session.deleteMany({
+      where: {
+        userId: passwordResetToken.userId,
+      }
+    });
+
+    const passwordHash = await hashPassword(password);
+    await prisma.user.update({
+      where: {
+        id: passwordResetToken.userId,
+      },
+      data: {
+        passwordHash,
+      },
+    });
   } catch (error) {
     return fromErrorToActionState(error, formData);
   }
